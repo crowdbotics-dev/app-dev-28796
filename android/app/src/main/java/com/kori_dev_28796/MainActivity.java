c117af130d25f54fc34af6b897778d441ac28802
Crowdbotics <engineering@crowdbotics.com>
2022-08-11 22:01:11 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/kori_dev_28796/MainActivity.java
@@ -0,0 +1,15 @@
+package com.kori_dev_28796;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "kori_dev_28796";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "kori_dev_28796",
+  url: "https://kori_dev_28796.botics.co",
+  api: "https://kori_dev_28796.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
